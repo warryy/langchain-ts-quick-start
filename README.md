5b9f9b89543d2d23aeebad9b921e150a5db238d7
wangyanyan <wangyanyan@kuaishou.com>
2024-02-20 14:50:25 +0800

    feat: llmchain&prompt template&format out to string

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # LangChain 学习 demo
+
+## 开始
+1. node 版本 >=18
+2. `pnpm i`
+3. 想查看 `src` 目录下的第 i 个 demo, 就在终端输入 `pnpm run dev:i`
+
+## demo
+### 1-hellow-world
+- hello world
+- 构建了一个 LLM Chain
+- prompt 模板功能
+- 结果格式化为字符串
+
+### 2-retrieval-chain
+- Retrieval Chain (检索链)
+- 增加检索链, 也就是相当于增加私域知识, 可以外挂文档
+- 对外挂文档进行文本分割(文本分割)
+- 将加载的文档进行向量存储(嵌入模型&向量存储)
--- src/1-hello-world/index.ts
+++ src/1-hello-world/index.ts
@@ -1,27 +1,53 @@
-require('dotenv').config()
+require("dotenv").config();
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 const chatModel = new ChatOpenAI({
   // 这是你的OpenAI API密钥，用于验证你的身份并允许你访问OpenAI的服务。如果环境变量里面有, 则不用传
   // openAIApiKey: process.env.OPENAI_API_KEY,
   // 这是你想要使用的模型的名称。在这个例子中，使用的是"gpt-3.5-turbo"模型。
   modelName: "gpt-3.5-turbo",
-  // 这个参数控制生成文本的随机性。值越高（接近1），生成的文本就越随机；值越低（接近0），生成的文本就越确定。
-  temperature: 0.5,
-  // 这个参数限制生成的文本的最大长度。在这个例子中，生成的文本最多包含100个 tokens。
-  maxTokens: 300,
-  // 这是一个用于控制生成文本的随机性的参数，也被称为nucleus sampling。它在每一步选择一个小集合的最可能的tokens，而不是在所有可能的tokens中选择。
-  topP: 1,
-  // 这个参数惩罚新出现的 tokens，使得模型更倾向于使用已经出现过的 tokens。
-  presencePenalty: 0.5,
-  // 这个参数惩罚频繁出现的 tokens，使得模型更倾向于使用不那么常见的 tokens。
-  frequencyPenalty: 0.5,
-  // 这个参数定义了生成文本时的停止条件。在这个例子中，当模型生成了一个换行符("\n")时，它会停止生成更多的文本。
+  // // 这个参数控制生成文本的随机性。值越高（接近1），生成的文本就越随机；值越低（接近0），生成的文本就越确定。
+  // temperature: 0.5,
+  // // 这个参数限制生成的文本的最大长度。在这个例子中，生成的文本最多包含100个 tokens。
+  // maxTokens: 300,
+  // // 这是一个用于控制生成文本的随机性的参数，也被称为nucleus sampling。它在每一步选择一个小集合的最可能的tokens，而不是在所有可能的tokens中选择。
+  // topP: 1,
+  // // 这个参数惩罚新出现的 tokens，使得模型更倾向于使用已经出现过的 tokens。
+  // presencePenalty: 0.5,
+  // // 这个参数惩罚频繁出现的 tokens，使得模型更倾向于使用不那么常见的 tokens。
+  // frequencyPenalty: 0.5,
+  // // 这个参数定义了生成文本时的停止条件。在这个例子中，当模型生成了一个换行符("\n")时，它会停止生成更多的文本。
   // stop: ["\n"],
 });
 
 const selfExecutingFn = async () => {
-  const res = await chatModel.invoke("what is LangSmith?");
-  console.log("%j", res);
+  // 1. hello world
+  // const res = await chatModel.invoke("what is LangSmith?");
+  // console.log("%j", res);
+
+  // 2. prompt template
+  // const prompt = ChatPromptTemplate.fromMessages([
+  //   ["system", "You are a world class technical documentation writer."],
+  //   ["user", "{input}"],
+  // ]);
+  // const chain = prompt.pipe(chatModel)
+  // const res = await chain.invoke({
+  //   input: "what is LangSmith",
+  // })
+  // console.log('%j', res)
+
+  // 3. 将输出对象转化为格式友好的字符串
+  const prompt = ChatPromptTemplate.fromMessages([
+    ["system", "You are a world class technical documentation writer."],
+    ["user", "{input}"],
+  ]);
+  const outputParser = new StringOutputParser();
+  const llmChain = prompt.pipe(chatModel).pipe(outputParser);
+  const res = await llmChain.invoke({
+    input: "what is LangSmith in LangChain",
+  });
+  console.log(res)
 };
 selfExecutingFn();
